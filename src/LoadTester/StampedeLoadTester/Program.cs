2a995a48a5c4000f5f91f64d41bc7e05b78bc3c2
Raul Palavecino <72534066+rbpalavecino@users.noreply.github.com>
2025-11-29 00:56:05 -0300

    Update Program.cs

--- src/LoadTester/StampedeLoadTester/Program.cs
+++ src/LoadTester/StampedeLoadTester/Program.cs
@@ -8,10 +8,39 @@ namespace StampedeLoadTester
     {
         const string OUTPUT_QUEUE = "BNA.XX1.PEDIDO";
         const string MENSAJE = "    00000008500000020251118115559N0001   000000PC  01100500000000000000                        00307384";
-        const int TIEMPO_CARGA_MS = 5000;
+        const int TIEMPO_CARGA_MS = 2000;
+        const string IP_MQ_SERVER = "10.6.248.10"; //"192.168.1.37"; //"192.168.0.15";
+        const string CHANNEL = "CHANNEL1";
+        
+        static readonly Hashtable _properties1414 = new()
+        {
+            { MQC.HOST_NAME_PROPERTY, IP_MQ_SERVER },
+            { MQC.PORT_PROPERTY, 1414 },
+            { MQC.CHANNEL_PROPERTY, "CHANNEL1" },
+        };
+
+        static readonly Hashtable _properties1415 = new()
+        {
+            { MQC.HOST_NAME_PROPERTY, IP_MQ_SERVER },
+            { MQC.PORT_PROPERTY, /*1415 */ 1414 },
+            { MQC.CHANNEL_PROPERTY, "CHANNEL1" },
+        };
+
+        static readonly Hashtable _properties1416 = new()
+        {
+            { MQC.HOST_NAME_PROPERTY, IP_MQ_SERVER },
+            { MQC.PORT_PROPERTY, /*1416*/ 1414 },
+            { MQC.CHANNEL_PROPERTY, "CHANNEL1" },
+        };
 
         static void Main(string[] args)
         {
+
+            using TestManager manager = new("MQGD", OUTPUT_QUEUE, MENSAJE, _properties1414, _properties1415, _properties1416);
+            manager.InicializarConexiones();
+
+            using MQQueue inquireQueue = manager.AbrirQueueInquire();
+            int profundidad = inquireQueue.CurrentDepth;
 
             if (args.Length > 0)
             {
@@ -29,7 +58,8 @@ namespace StampedeLoadTester
                     // En otro equipo (cliente)
                     Console.WriteLine("Iniciando cliente...");
                     //TimeSpan? responseTime = remoteManager.Ping("192.168.0.15", 8888, TimeSpan.FromSeconds(5));
-                    TimeSpan? responseTime = remoteManager.Ping("192.168.56.1", 8888, TimeSpan.FromSeconds(5));
+                    //TimeSpan? responseTime = remoteManager.Ping("192.168.56.1", 8888, TimeSpan.FromSeconds(5));
+                    TimeSpan? responseTime = remoteManager.Ping("10.7.232.88", 8888, TimeSpan.FromSeconds(5));
                     if (responseTime.HasValue)
                     {
                         Console.WriteLine($"Tiempo de respuesta: {responseTime.Value.TotalMilliseconds} ms");
@@ -43,36 +73,15 @@ namespace StampedeLoadTester
 
             Console.WriteLine("Iniciando...");
 
-            const string IP_MQ_SERVER = "192.168.1.37"; //"192.168.0.15";
-            const string CHANNEL = "CHANNEL1";
 
-            var properties1414 = new Hashtable
-            {
-                { MQC.HOST_NAME_PROPERTY, IP_MQ_SERVER },
-                { MQC.PORT_PROPERTY, 1414 },
-                { MQC.CHANNEL_PROPERTY, "CHANNEL1" },
-            };
 
-            var properties1415 = new Hashtable
-            {
-                { MQC.HOST_NAME_PROPERTY, IP_MQ_SERVER },
-                { MQC.PORT_PROPERTY, 1415 },
-                { MQC.CHANNEL_PROPERTY, "CHANNEL1" },
-            };
-
-            var properties1416 = new Hashtable
-            {
-                { MQC.HOST_NAME_PROPERTY, IP_MQ_SERVER },
-                { MQC.PORT_PROPERTY, 1416 },
-                { MQC.CHANNEL_PROPERTY, "CHANNEL1" },
-            };
-
+/*
             using TestManager manager = new("MQGD", OUTPUT_QUEUE, MENSAJE, properties1414, properties1415, properties1416);
             manager.InicializarConexiones();
 
             using MQQueue inquireQueue = manager.AbrirQueueInquire();
             int profundidad = inquireQueue.CurrentDepth;
-
+*/
 /*
 int inquireCounter = 0;
             long tiempoLimiteinquire = (long)(TimeSpan.FromMilliseconds(10000).TotalSeconds * Stopwatch.Frequency);
@@ -100,7 +109,7 @@ int inquireCounter = 0;
 
             manager.EnviarMensajesPrueba();
 
-            int numHilos = 4;//Environment.ProcessorCount;
+            int numHilos = 6;//Environment.ProcessorCount;
             Console.WriteLine($"Número de hilos: {numHilos}");
             Console.WriteLine($"Profundidad inicial: {profundidad}");
 
