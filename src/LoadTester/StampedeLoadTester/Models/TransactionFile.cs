76902470baafc44a5cdb18ef831eb26efc3e117e
Raul Palavecino <72534066+rbpalavecino@users.noreply.github.com>
2026-01-14 18:37:22 -0300

    limpieza de clases

---
+++ src/LoadTester/StampedeLoadTester/Models/TransactionFile.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StampedeLoadTester.Models
+{
+    internal class TransactionFile
+    {
+        [JsonPropertyName("version")]
+        public int Version { get; set; }
+
+        [JsonPropertyName("inputQueue")]
+        public string InputQueue { get; set; } = string.Empty;
+
+        [JsonPropertyName("outputQueue")]
+        public string OutputQueue { get; set; } = string.Empty;
+
+        [JsonPropertyName("transacciones")]
+        public List<string> Transacciones { get; set; } = [];
+    }
+}
